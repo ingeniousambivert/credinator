34d3812da4e4d67a7fb559daba15c5a8b9eb6fd5
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-04-24 11:47:40 +0530

    refactor component

--- web/src/components/forms/verify.js
+++ web/src/components/forms/verify.js
@@ -58,7 +58,7 @@ export default function VerifyForm(props) {
               type="number"
               id="number"
               name="number"
-              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Enter Card Number"
             />
           </div>

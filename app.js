84220ae118057ea5c4c38ad246de7026eaf1d9d2
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2021-04-24 12:15:50 +0530

    add catchall route handler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const path = require("path");
 
 const indexRouter = require("./routes");
 const generatorRouter = require("./routes/generator");
@@ -13,8 +14,10 @@ app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use("/", indexRouter);
 app.use("/generator", generatorRouter);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 module.exports = app;
